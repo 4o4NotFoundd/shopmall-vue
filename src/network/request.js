d6ee947fc6728a74feecf2086764df035bea9c71
4o4NotFoundd <2812645290@qq.com>
2020-03-11 10:54:18 +0800

    分类

--- src/network/request.js
+++ src/network/request.js
@@ -8,7 +8,8 @@ import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
     // baseURL: 'http://106.54.54.237:8000/api/w1',
-    baseURL: 'http://106.54.54.237:8000/api/hy',
+    baseURL:'http://106.54.54.237:8000/api/hy',
+    // baseURL: 'http://106.54.54.237:8000/api/hy',
     //来源：1月13 https://github.com/18849042545/supermall/blob/master/src/network/requerst.js
 
     //备用
